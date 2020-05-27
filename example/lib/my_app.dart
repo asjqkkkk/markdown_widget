187fc38601f26a02c3d5b20c8cee5c4291ec5c1f
老晨子 <772565130@qq.com>
2020-05-27 11:03:14 +0800

    add theme button in example

--- example/lib/my_app.dart
+++ example/lib/my_app.dart
@@ -1,20 +1,28 @@
+import 'package:easy_model/easy_model.dart';
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Markdown Demo',
-      theme: ThemeData(
-          primarySwatch: Colors.blue,
-          brightness: isDarkNow ? Brightness.dark : Brightness.light),
-      home: HomePage(),
+    return ModelWidget<GlobalModel>(
+      modelBuilder: () => GlobalModel(),
+      childBuilder: (ctx, model){
+        final brightness = model.brightness;
+        model = null;
+        return MaterialApp(
+          title: 'Markdown Demo',
+          theme: ThemeData(
+              primarySwatch: Colors.blue,
+              brightness: brightness),
+          home: HomePage(),
+        );
+      }
     );
   }
 }
 
-bool get isDarkNow {
-  final int curHour = DateTime.now().hour;
-  return curHour > 18 || curHour < 7;
+class GlobalModel extends Model{
+  Brightness brightness = Brightness.light;
 }
+
