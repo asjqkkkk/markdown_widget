e73ba222113a7910f9ea0abc224301297c3f8b1b
kuro <7445841+kuro7766@user.noreply.gitee.com>
2021-03-25 19:30:16 +0800

    fix tag parse

--- lib/tags/other.dart
+++ lib/tags/other.dart
@@ -6,11 +6,14 @@ import 'package:markdown/markdown.dart' as m;
 InlineSpan getOtherWidgetSpan(m.Element node) =>
     WidgetSpan(child: getOtherWidget(node));
 
-///the custom widget
 Widget getOtherWidget(m.Element node) {
   final customWidget = StyleConfig().pConfig?.custom;
-  final m.Element customNode = node.children[0];
+
+  final customNode =
+  m.Element((node.children[0] as m.Element).tag, node.children);
+
   if (customWidget != null) {
+    // return customWidget.call(customNode);
     return customWidget.call(customNode);
   } else {
     debugPrint('UnCatch Node:${customNode.tag}');
