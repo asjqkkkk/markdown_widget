a074676e43b57e7acd3dd2b4fe34cf6ea3d20c47
asjqkkkk <agedchen@gmail.com>
2021-05-08 13:16:36 +0000

    update

---
+++ assets/assets/demo_en.md
@@ -0,0 +1,212 @@
+Language:[简体中文](https://github.com/asjqkkkk/markdown_widget/blob/master/README_ZH.md)|[English](https://github.com/asjqkkkk/markdown_widget/blob/master/README.md)
+
+# 📖markdown_widget
+
+[![support](https://img.shields.io/badge/platform-flutter%7Cdart%20vm-ff69b4.svg?style=flat-square)](https://github.com/asjqkkkk/markdown_widget)
+[![Flutter Web](https://github.com/asjqkkkk/markdown_widget/workflows/Flutter%20Web/badge.svg)](https://github.com/asjqkkkk/markdown_widget/actions)
+[![pub package](https://img.shields.io/pub/v/markdown_widget.svg)](https://pub.dartlang.org/packages/markdown_widget)
+[![demo](https://img.shields.io/badge/demo-online-brightgreen)](https://oldchen.top/markdown_widget/)
+
+A simple and easy-to-use markdown package created by flutter.
+
+- Support TOC
+- Support img and Video Tags of HTML
+- Support highlight code
+
+
+## 🚀Getting Started
+
+Before the introduction,  you can have a try for [Online Demo](https://oldchen.top/markdown_widget/)
+
+### 🔑Useage
+
+
+```
+import 'package:flutter/material.dart';
+import 'package:markdown_widget/markdown_widget.dart';
+
+class MarkdownPage extends StatelessWidget {
+  final String data;
+
+  MarkdownPage(this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.all(10),
+        child: buildMarkdown(),
+      ),
+    );
+  }
+
+  Widget buildMarkdown() => MarkdownWidget(data: data);
+}
+```
+
+if you want to use column or other list widget, you can use `MarkdownGenerator`
+
+
+```
+  Widget buildMarkdown() => Column(children: MarkdownGenerator(data: data).widgets,);
+```
+
+## 🌠Dark theme
+
+`markdown_widget` supports dark mode by default，you can use it by setting the `markdownTheme` of `StyleConfig`
+
+```
+  Widget buildMarkdown() => MarkdownWidget(
+        data: data,
+        controller: controller,
+        styleConfig: StyleConfig(
+          markdownTheme: MarkdownTheme.lightTheme
+        ),
+      );
+```
+
+<img src="https://user-images.githubusercontent.com/30992818/79996264-d476f180-84ea-11ea-8ea2-b82a85b8c6db.png" width=400> <img src="https://user-images.githubusercontent.com/30992818/79996304-e6589480-84ea-11ea-950a-5c4fb89c1ad3.png" width=400>
+
+you can also custom your own `markdownTheme`
+
+
+## 🏞Image and Video
+
+if you want to custom a widget, such as **img** and **video**:
+
+```
+  Widget buildMarkdown() => MarkdownWidget(
+        data: data,
+        styleConfig: StyleConfig(
+          imgBuilder: (String url,attributes) {
+            return Image.network(url);
+          },
+          videoBuilder: (String url,attributes) {
+            return YourVideoWidget();
+          }
+        ),
+      );
+```
+
+supported markdown samples:
+
+```
+<video src="https://xxx.mp4" controls="controls"/>
+
+<img width="150" alt="018" src="https://xxx.png"/>
+
+![demo](https://xxx)
+
+```
+
+if you want to custom other tag widgets, you need use `WidgetConfig`
+
+## 🔗Links
+
+you can custom link style
+
+
+```
+  Widget buildMarkdown() => MarkdownWidget(
+        data: data,
+        styleConfig: StyleConfig(
+          pConfig: PConfig(
+            linkStyle: TextStyle(...),
+            onLinkTap: (url){
+              _launchUrl(url);
+            }
+          )
+        ),
+      );
+```
+
+## 🍑Custom Tag
+
+you can use custom tag like this
+
+```markdown
+<avatar size="12" name="tom" />
+```
+
+then add `custom` like this
+
+```dart
+      MarkdownWidget(
+        data: data,
+            styleConfig: StyleConfig(
+              pConfig: PConfig(
+                custom: (m.Element node) {
+                  ...
+                  return YourCustomWidget();
+                },
+              ),
+            ),
+          ),
+```
+
+## 📜TOC
+
+it's very easy to use TOC
+
+```
+  final TocController tocController = TocController();
+
+  Widget buildTocWidget() => TocListWidget(controller: controller);
+
+  Widget buildMarkdown() => MarkdownWidget(data: data, controller: controller);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Row(
+      children: <Widget>[
+        Expanded(child: buildTocWidget()),
+        Expanded(child: buildMarkdown(), flex: 3)
+      ],
+    ));
+  }
+```
+
+## 🎈hightlight code
+
+you can config lots of theme for code
+
+```
+import 'package:markdown_widget/config/highlight_themes.dart' as theme;
+
+  Widget buildMarkdown() => MarkdownWidget(
+        data: data,
+        styleConfig: StyleConfig(
+          preConfig: PreConfig(
+            language: 'java',
+            theme: theme.a11yLightTheme
+          )
+        ),
+      );
+```
+
+if you have any good idea or sugesstion, [welcome for PR and issue](https://github.com/asjqkkkk/markdown_widget)
+
+
+# appendix
+
+Here are the other packages used in markdown_widget
+
+
+package | explain
+---|---
+[markdown](https://pub.flutter-io.cn/packages/markdown) | parse markdown data
+[flutter_highlight](https://pub.flutter-io.cn/packages/flutter_highlight) | make code highlight
+[html](https://pub.flutter-io.cn/packages/html) | parse html data
+[video_player_web](https://pub.flutter-io.cn/packages/video_player_web) | play video in flutter web
+[video_player](https://pub.flutter-io.cn/packages/video_player) | video interface
+[scrollable_positioned_list](https://pub.flutter-io.cn/packages/scrollable_positioned_list) | for TOC function
+
+## The reason why I created this package
+
+Since there is already [flutter_markdown](https://pub.flutter-io.cn/packages/flutter_markdown) package, why should I create another one?
+
+Because when I was creating a [personal blog](https://oldchen.top/flutter-blog/#/) using flutter web, I found that flutter_markdown has many functions that are not available
+
+ I have submitted [3 issues](https://github.com/flutter/flutter_markdown/issues/188) without any responding. So I have to create a new one.
+
---
+++ flutter_service_worker.js
@@ -0,0 +1,193 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "assets/assets/demo_zh.md": "6336c7b8b50e027bf2309e3730d14b8c",
+"assets/assets/demo_en.md": "aab031c5f9214f2b9276682fabca5f09",
+"assets/assets/editor.md": "a141965a59ebc1620c6e17aef2c204bf",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/AssetManifest.json": "204a0c9691dbc382013cacbd8f9e2c6b",
+"assets/NOTICES": "1e38bd93879f6ebd7be61299e6a64789",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"version.json": "4b6db237b3514a88107a422469adfb0f",
+"manifest.json": "00e0b69b49487ce4f9ff0c5fac8fda49",
+"main.dart.js": "1ba48e1428b0430caaf7c2194bf42c07",
+"index.html": "4ab3cd01cbb759cc85cd4e03423ff81f",
+"/": "4ab3cd01cbb759cc85cd4e03423ff81f"
+};
+
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = [
+  "/",
+"main.dart.js",
+"index.html",
+"assets/NOTICES",
+"assets/AssetManifest.json",
+"assets/FontManifest.json"];
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache.
+        return response || fetch(event.request).then((response) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+    })
+  );
+});
+
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
