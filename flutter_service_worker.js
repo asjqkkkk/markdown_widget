046b5fbb68c633c3fdf81febd3cae5ea59e24979
asjqkkkk <asjqkkkk@users.noreply.github.com>
2023-01-07 05:08:27 +0000

    deploy: 761a149ba49325bafaa54ee4208171f7215387f1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,8 +23,8 @@ const RESOURCES = {
 "assets/assets/demo_zh.md": "6336c7b8b50e027bf2309e3730d14b8c",
 "assets/assets/editor.md": "b263c53132d968f60ec3a03698dc1265",
 "assets/NOTICES": "1f4277c7f6ab958ccffbe9c98d813df6",
-"index.html": "38be7a76707a9ce54c69b675c7908bcf",
-"/": "38be7a76707a9ce54c69b675c7908bcf",
+"index.html": "58c1cce64a7ebd762f585046a45a60c7",
+"/": "58c1cce64a7ebd762f585046a45a60c7",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "ff966ab969ba381b900e61629bfb9789"
 };
