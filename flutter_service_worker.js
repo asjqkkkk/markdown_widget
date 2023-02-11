122ada3a5e29d04f4aab66dad745e3d98383bb9f
asjqkkkk <asjqkkkk@users.noreply.github.com>
2023-02-11 15:57:42 +0000

    deploy: 3b84444e38c5633c6cc11ce20b53cc5a291e60ca

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
-"index.html": "3734cfb0fb19b2accda2a44ad9382d84",
-"/": "3734cfb0fb19b2accda2a44ad9382d84",
-"main.dart.js": "a49d62921ffdefa1a85d6a914aa8674a",
+"index.html": "2c61611a94aa02179c0ab24e353d7ae4",
+"/": "2c61611a94aa02179c0ab24e353d7ae4",
+"main.dart.js": "13258093044658d2251596d4f3f9e610",
 "manifest.json": "00e0b69b49487ce4f9ff0c5fac8fda49",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
