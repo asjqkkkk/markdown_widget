870680df5f0166e094bae56e988ce6be2691e890
bin <17426470+boyan01@users.noreply.github.com>
2023-03-01 22:33:49 +0800

    add a workaround

--- lib/widget/blocks/leaf/link.dart
+++ lib/widget/blocks/leaf/link.dart
@@ -20,15 +20,17 @@ class LinkNode extends ElementNode {
   @override
   InlineSpan build() {
     final url = attributes['href'] ?? '';
-    return TextSpan(
-      children: List.generate(children.length, (index) {
-        final child = children[index];
-        return _toLinkInlineSpan(
+    return TextSpan(children: [
+      for (final child in children)
+        _toLinkInlineSpan(
           child.build(),
           () => _onLinkTap(linkConfig, url),
-        );
-      }),
-    );
+        ),
+      if (children.isNotEmpty)
+        // FIXME: this is a workaround, maybe need fixed by flutter framework.
+        // add a space to avoid the space area of line end can be tapped.
+        TextSpan(text: ' '),
+    ]);
   }
 
   void _onLinkTap(LinkConfig linkConfig, String url) {
@@ -67,6 +69,11 @@ InlineSpan _toLinkInlineSpan(InlineSpan span, VoidCallback onTap) {
       children: span.children?.map((e) => _toLinkInlineSpan(e, onTap)).toList(),
       style: span.style,
       recognizer: TapGestureRecognizer()..onTap = onTap,
+      onEnter: span.onEnter,
+      onExit: span.onExit,
+      semanticsLabel: span.semanticsLabel,
+      locale: span.locale,
+      spellOut: span.spellOut,
     );
   } else if (span is WidgetSpan) {
     span = WidgetSpan(
